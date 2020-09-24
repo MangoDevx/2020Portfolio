0c3ce700b6a3747d6cfeea6fe0b2b0f55b0c8120
Keith Ritter <ritter.keithpersonal@gmail.com>
2020-09-24 04:25:28 -0400

    Updated contact, composer, and api

--- html/contact.php
+++ html/contact.php
@@ -1,27 +1,17 @@
 <?php
-    $api_key = "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";
 
 require 'vendor/autoload.php';
 use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-$emailer = $_POST['email'];
-$subject = $_POST['subject'];
-$body = $_POST['body'];
-$message = "From: $emailer\n" . "Message: $message";
-
 
 $email = new \SendGrid\Mail\Mail(); 
 $email->setFrom("keithritterportfolio@gmail.com", "Portfolio Inquiry");
-$email->setSubject($subject);
+$email->setSubject("subject");
 $email->addTo("keithritterportfolio@gmail.com", "Portfolio Inquiry");
-$email->addContent("text/plain", $message);
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid($_ENV["SENDGRID_API_KEY"]);
+$email->addContent("text/plain", "Test works");
+
+$sendgrid = new \SendGrid($_ENV["sendgrid"]);
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
